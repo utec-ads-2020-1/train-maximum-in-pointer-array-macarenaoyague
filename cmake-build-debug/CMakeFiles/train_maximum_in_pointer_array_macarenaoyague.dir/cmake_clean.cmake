eafed4332d9a94de68d97779c52c62d2488c889d
Macarena <macarena.oyague@utec.edu.pe>
2020-03-26 01:07:36 -0500

    Update

---
+++ cmake-build-debug/CMakeFiles/train_maximum_in_pointer_array_macarenaoyague.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train_maximum_in_pointer_array_macarenaoyague.dir/main.cpp.o"
+  "train_maximum_in_pointer_array_macarenaoyague.pdb"
+  "train_maximum_in_pointer_array_macarenaoyague"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/train_maximum_in_pointer_array_macarenaoyague.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int max(int *iptr[], int n);
+
+int main() {
+    int n;
+    int array[100]={};
+    int *iptr[100]={};
+    cin>>n;
+    for (int i = 0; i < n; i++) {
+        cin>>array[i];
+        iptr[i] = &array[i];
+    }
+    cout<<max(iptr, n);
+    return 0;
+}
+
+int max(int *iptr[], int n){
+
+    int max = 0;
+
+    for (int i=0; i<n; i++)
+        if (*iptr[i]>max)
+            max=*iptr[i];
+
+    return max;
+}
+
+//is missing the limit n≤100
